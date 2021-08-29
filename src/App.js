8245356160ab383123da2bb45ab9135c9bdfde3e
Nekit <d3m0nhunt3r92@gmailcom>
2021-08-29 18:10:01 +0300

    home work 7

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import {useReducer} from "react";
+import reducer from "./reducers/reducer";
 
 export default function UserDiv() {
+
+    const [{a, b, c}, dispatch] = useReducer(reducer,{a:0,b:0,c:0});
+
     return (
         <div>
-
+            <h2>a - {a}</h2>
+            <button onClick={() => dispatch({obj: '1', diya: '+'})}>increment</button>
+            <button onClick={() => dispatch({obj: '1', diya: '-'})}>decrement</button>
+            <h2>b - {b}</h2>
+            <button onClick={() => dispatch({obj: '2', diya: '+'})}>increment</button>
+            <button onClick={() => dispatch({obj: '2', diya: '-'})}>decrement</button>
+            <h2>c - {c}</h2>
+            <button onClick={() => dispatch({obj: '3', diya: '+'})}>increment</button>
+            <button onClick={() => dispatch({obj: '3', diya: '-'})}>decrement</button>
         </div>
     );
 }
