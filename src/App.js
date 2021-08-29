a23d3cb18a7957bec82330d77569a669c99305a1
Nekit <d3m0nhunt3r92@gmailcom>
2021-08-29 17:14:56 +0300

    home work 7

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import {useReducer} from "react";
 
-export default function UserDiv() {
+const reducer = (state, action) => {
+    switch (action) {
+        case '+':
+            return state + 10;
+        case '-':
+            return state - 2;
+        default:
+            return state;
+    }
+}
+
+export default function App() {
+    const [state, dispatch] = useReducer(reducer, 0);
+
     return (
         <div>
-
+            <h2>state - {state}</h2>
+            <button onClick={() => dispatch('+')}>inc state by 10</button>
+            <button onClick={() => dispatch('-')}>dec state by 2</button>
         </div>
     );
 }
